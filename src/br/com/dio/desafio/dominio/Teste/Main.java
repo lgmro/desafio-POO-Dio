b41914466f6fd4dae66676b2e310fe9ac4814457
lgmro <lucasgmmro@gmail.com>
2022-01-30 23:03:23 -0300

    Criação de classe

---
+++ src/br/com/dio/desafio/dominio/Teste/Main.java
@@ -0,0 +1,24 @@
+package br.com.dio.desafio.dominio.Teste;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentorias;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Teste");
+        curso1.setDescricao("Descrição");
+        curso1.setCargaHoraria(10);
+
+        Mentorias mentoria = new Mentorias();
+        mentoria.setTitulo("Java");
+        mentoria.setDescricao("Aula");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(mentoria);
+    }
+}
