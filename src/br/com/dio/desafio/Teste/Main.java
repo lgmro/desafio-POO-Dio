de7c01e00b4fe037ecb16e1928bc9cf9a138c1b0
lgmro <lucasgmmro@gmail.com>
2022-02-02 18:21:25 -0300

    Final.

--- src/br/com/dio/desafio/Teste/Main.java
+++ src/br/com/dio/desafio/Teste/Main.java
@@ -1,6 +1,8 @@
 package br.com.dio.desafio.Teste;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentorias;
 
 import java.time.LocalDate;
@@ -18,7 +20,36 @@ public class Main {
         mentoria.setDescricao("Aula");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(mentoria);
+//        System.out.println(curso1);
+//        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devLucas = new Dev();
+        devLucas.setNome("Lucas Gabriel");
+        devLucas.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos: " + devLucas.getConteudosInscritos());
+        devLucas.progedir();
+        System.out.println("Conteúdos Concluídos: " + devLucas.getConteudosConcluidos());
+        System.out.println("Conteúdos Inscritos: " + devLucas.getConteudosInscritos());
+        System.out.println("XP: " + devLucas.calcularTotalXp());
+
+        System.out.println("--------------");
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos: " + devCamila.getConteudosInscritos());
+        devCamila.progedir();
+        devCamila.progedir();
+        System.out.println("Conteúdos Concluídos: " + devCamila.getConteudosConcluidos());
+        System.out.println("Conteúdos Inscritos: " + devCamila.getConteudosInscritos());
+        System.out.println("XP: " + devCamila.calcularTotalXp());
+
+
     }
 }
